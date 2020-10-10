a890a928c8251cc5a4cfd0eb611c7a0b4c6cb20d
Steedalion <alex.steed88@gmail.com>
2020-10-10 11:35:39 +0900

    Prototyping complete

--- Assets/Scripts/MouseManager.cs
+++ Assets/Scripts/MouseManager.cs
@@ -6,51 +6,71 @@ using UnityEngine.Events;
 public class MouseManager : MonoBehaviour
 {
 
-    public LayerMask clickableLayer;
+       public LayerMask clickableLayer;
 
-    public Texture2D normalCursor;
-    public Texture2D targetCursor;
-    public Texture2D doorwayCursor;
-    public Texture2D combatCursor;
+       public Texture2D normalCursor;
+       public Texture2D targetCursor;
+       public Texture2D doorwayCursor;
+       public Texture2D combatCursor;
 
-    public EventVector3 onClickEnvironment;
-    // Start is called before the first frame update
-    void Start()
-    {
+       public EventVector3 onClickEnvironment;
+          //Start is called before the first frame update
+       void Start()
+       {
         
-    }
+       }
 
-    // Update is called once per frame
-    void Update()
-    {
-        RaycastHit hit;
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, clickableLayer))
-        {
-            bool door = false;
+          //Update is called once per frame
+       void Update()
+       {
+           RaycastHit hit;
+           if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, clickableLayer))
+           {
+               bool door = false;
+               bool item = false;
+               bool enemy = false;
 
-            if (hit.collider.gameObject.tag == "Doorway")
-            {
-                Cursor.SetCursor(doorwayCursor, new Vector2(16, 16), CursorMode.Auto);
-                door = true;
-            }
+               if (hit.collider.gameObject.tag == "Doorway")
+               {
+                   Cursor.SetCursor(doorwayCursor, new Vector2(16, 16), CursorMode.Auto);
+                   door = true;
+               }
+               else if (hit.collider.gameObject.tag == "Item")
+               {
+                   Cursor.SetCursor(targetCursor, new Vector2(16, 16), CursorMode.Auto);
+                   item = true;
+               }
 
-            else
-            {
-                Cursor.SetCursor(targetCursor, new Vector2(16, 16), CursorMode.Auto);
-            }
+               else
+               {
+                   Cursor.SetCursor(normalCursor, new Vector2(16, 16), CursorMode.Auto);
+               }
 
-            if (Input.GetMouseButtonDown(0))
-            {
-                onClickEnvironment.Invoke(hit.point);
-            }
+                  if (Input.GetMouseButtonDown(0))
+                  {
+                         if(door)
+                         {
+                               Transform doorway = hit.collider.gameObject.transform;
+                             onClickEnvironment.Invoke(hit.point);
+                             Debug.Log("Doorway");  
+                         }if(item){
+                             Transform doorway = hit.collider.gameObject.transform;
+                             onClickEnvironment.Invoke(hit.point);
+                             Debug.Log("Item");
+                         }
+                         else{
+                         onClickEnvironment.Invoke(hit.point);
+                         }
+                         
+                  }
 
 
-        }
-        else
-        {
-            Cursor.SetCursor(normalCursor, Vector2.zero, CursorMode.Auto);
-        }
-    }
+           }
+           else
+           {
+               Cursor.SetCursor(normalCursor, Vector2.zero, CursorMode.Auto);
+           }
+       }
 }
 
 [System.Serializable]
---
+++ Assets/Scripts/NPCController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class NPCController : MonoBehaviour
+{
+    public float patrolTime = 5f;
+    public float agroRange = 10f;
+    public Transform[] waypoints;
+
+    private int index;
+    private float speed, agentSpeed;
+    Transform player;
+    Animator animator;
+    private NavMeshAgent agent;
+    void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        if(agent != null) {agentSpeed = agent.speed;}
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        index = Random.Range(0, waypoints.Length); //start at random point
+
+        InvokeRepeating("Tick", 0, 0.5f);
+
+
+        if (waypoints.Length > 1)
+        {
+            InvokeRepeating("Patrol", 0, patrolTime);
+        }
+    }
+        // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Patrol()
+    {
+        index = index == waypoints.Length -1 ? 0 : index +1;
+    }
+    void Tick()
+    {
+        agent.destination = waypoints[index].position;
+        agent.speed = agentSpeed / 2;
+
+        if(player != null && Vector3.Distance(player.position, transform.position) < agroRange)
+        {
+            agent.destination = player.position;
+            agent.speed = agentSpeed;
+        }
+    }
+}
